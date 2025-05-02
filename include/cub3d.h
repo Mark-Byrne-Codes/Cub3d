c2a4465b3b79c4e0762ef9ecff63f2cc8341bfe7
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-05-02 17:00:42 +0300

    created parsing.c file and some initial code

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,11 +1,27 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <unistd.h>
 # include <fcntl.h>
-# include <stdlib.h>
 # include <math.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_index
+{
+	int		i;
+	int		j;
+} t_index;
+
+typedef struct s_map
+{
+	int		fd;
+	int		max_width;
+	int		max_height;
+	char	*file_path;
+	t_index	*index;
+} t_map;
 
 
+int run_game(argv);
 #endif
